ca0372c605ffac0556f08ac1cd0b43c0ce3643cb
ᴍʏʟᴏᴠᴇᴍᴏᴏɴ <90930470+MyLoveMoon@users.noreply.github.com>
2023-04-08 22:51:40 +0530

    Update main.py

--- cache/main.py
+++ cache/main.py
@@ -17,7 +17,7 @@ priyamusic = Client(
     
     )
 
-user = PyTgCalls(esport,
+user = PyTgCalls(priyamusic,
     cache_duration=100,
     overload_quiet_mode=True,)
 
@@ -25,5 +25,5 @@ call_py = PyTgCalls(esport, overload_quiet_mode=True)
 
 with Client("PriyaMusic", API_ID, API_HASH, bot_token=BOT_TOKEN) as app:
     me_bot = app.get_me()
-with esport as app:
-    me_esport = app.get_me()
+with priyamusic as app:
+    me_priyamusic = app.get_me()
